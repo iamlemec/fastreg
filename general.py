d72673b1b896090581264a74019c00a17217c003
Douglas Hanley <thesecretaryofwar@gmail.com>
2019-12-10 00:49:18 -0500

    things have been properly factored

--- general.py
+++ general.py
@@ -8,8 +8,37 @@ from torch.autograd import grad
 
 from .design import design_matrices
 
-# constants
+##
+## constants
+##
+
 eps = 1e-7
+
+##
+## tools
+##
+
+def param_table(beta, sigma, names):
+    # standard errors
+    stderr = np.sqrt(sigma.diagonal())
+
+    # confidence interval
+    s95 = norm.ppf(0.975)
+    low95 = beta - s95*stderr
+    high95 = beta + s95*stderr
+
+    # p-value
+    zscore = beta/stderr
+    pvalue = 1 - norm.cdf(np.abs(zscore))
+
+    # return all
+    return pd.DataFrame({
+        'coeff': beta,
+        'stderr': stderr,
+        'low95': low95,
+        'high95': high95,
+        'pvalue': pvalue
+    }, index=names)
 
 ##
 ## glm with torch
@@ -25,47 +54,20 @@ def hessian(y, x, device=None):
     rows = [flatgrad(dy, x, grad_outputs=u, retain_graph=True) for u in units]
     return torch.stack(rows)
 
-# generic GLM model
-class GlmModel(torch.nn.Module):
-    def __init__(self, K, link):
-        super().__init__()
-        self.linear = torch.nn.Linear(K, 1, bias=False)
-        self.link = link
-
-    def forward(self, x):
-        x = torch.flatten(self.linear(x))
-        x = self.link(x)
-        return x
-
-# default specification
-link0 = lambda x: x
-loss0 = lambda i, o: torch.pow(i-o, 2)
-
-# glm regression using torch
-def glm(y, x=[], fe=[], data=None, intercept=True, drop='first', link_fn=link0, loss_fn=loss0, batch_size=4092, epochs=3, learning_rate=0.5):
-    if len(x) == 0 and len(fe) == 0 and not intercept:
-        raise(Exception('No columns present!'))
-
-    # choose best device
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    # construct design matrices
-    y_vec, x_mat, x_names = design_matrices(y, x, fe, data, intercept=intercept, drop=drop)
-    N, K = x_mat.shape
+# maximum likelihood using torch
+def maxlike(y, x, model, params, batch_size=4092, epochs=3, learning_rate=0.5, dtype=np.float32, device='cpu'):
+    # get data size
+    N = len(y)
 
     # convert to tensors
-    y_ten = torch.from_numpy(y_vec.astype(np.float32)).to(device)
-    x_ten = torch.from_numpy(x_mat.astype(np.float32)).to(device)
+    y_ten = torch.from_numpy(y.astype(dtype)).to(device)
+    x_ten = torch.from_numpy(x.astype(dtype)).to(device)
 
     # create dataset and dataloader
     dset = TensorDataset(x_ten, y_ten)
     dlod = DataLoader(dset, batch_size, shuffle=True)
 
-    # construct model
-    model = GlmModel(K, link_fn).to(device)
-
     # create optimizer
-    params = [model.linear.weight]
     optim = torch.optim.SGD(params, lr=learning_rate)
 
     # do training
@@ -76,9 +78,7 @@ def glm(y, x=[], fe=[], data=None, intercept=True, drop='first', link_fn=link0, 
         # iterate over batches
         for x_bat, y_bat in dlod:
             # compute gradients
-            pred = model(x_bat)
-            lvec = loss_fn(pred, y_bat)
-            loss = torch.mean(lvec)
+            loss = model(y_bat, x_bat)
             loss.backward()
 
             # implement update
@@ -93,44 +93,67 @@ def glm(y, x=[], fe=[], data=None, intercept=True, drop='first', link_fn=link0, 
         avg_loss = agg_loss/agg_batch
         print(f'{ep:3}: loss = {avg_loss}')
 
-    # construct params
-    betas = model.linear.weight.detach().cpu().numpy().flatten()
-    table = pd.DataFrame({'coeff': betas}, index=x_names)
+    # construct params (flatified)
+    beta = [p.detach().cpu().numpy().flatten() for p in params]
 
-    # get hessian
-    fisher = np.zeros((K, K))
+    # get hessian (flatified)
+    K = [p.numel() for p in params]
+    fisher = [np.zeros((s, s)) for s in K]
     for x_bat, y_bat in dlod:
-        pred = model(x_bat)
-        lvec = loss_fn(pred, y_bat)
-        loss = torch.mean(lvec)
-        hess = hessian(loss, model.linear.weight, device=device)
-        fisher += hess.detach().cpu().numpy()
-    fisher *= batch_size/N
+        loss = model(y_bat, x_bat)
+        for i, p in enumerate(params):
+            hess = hessian(loss, p, device=device)
+            fisher[i] += hess.detach().cpu().numpy()
+    for i in range(len(params)):
+        fisher[i] *= batch_size/N
 
     # get cov matrix
-    cov = np.linalg.inv(fisher)/N
-    stderr = np.sqrt(cov.diagonal())
-
-    # confidence interval
-    s95 = norm.ppf(0.975)
-    low95 = betas - s95*stderr
-    high95 = betas + s95*stderr
-
-    # p-value
-    zscore = betas/stderr
-    pvalue = 1 - norm.cdf(np.abs(zscore))
-
-    # store for return
-    table['stderr'] = stderr
-    table['low95'] = low95
-    table['high95'] = high95
-    table['pvalue'] = pvalue
+    sigma = [np.linalg.inv(f)/N for f in fisher]
 
     # return all
-    return table
+    return beta, sigma
 
-# standard poisson regression
-def poisson(y, x=[], fe=[], data=None, **kwargs):
+# default specification
+link0 = lambda x: x
+loss0 = lambda i, o: torch.pow(i-o, 2)
+def glm(y, x, data, link=link0, loss=loss0, params=[], intercept=True, drop='first', device='cpu', output='single', **kwargs):
+    if len(x) == 0 and len(fe) == 0 and not intercept:
+        raise(Exception('No columns present!'))
+
+    # construct design matrices
+    y_vec, x_mat, x_names = design_matrices(y, x, data=data, intercept=intercept, drop=drop)
+    N, K = x_mat.shape
+
+    # linear layer
+    linear = torch.nn.Linear(K, 1, bias=False).to(device)
+
+    # collect params
+    params = [linear.weight] + params
+
+    # evaluator
+    def model(y, x):
+        inter = torch.flatten(linear(x))
+        pred = link(inter)
+        like = loss(pred, y)
+        return torch.mean(like)
+
+    # estimate model
+    beta, sigma = maxlike(y_vec, x_mat, model, params, device=device, **kwargs)
+
+    # interpret results
+    if output == 'full':
+        return beta, sigma
+    elif output == 'single':
+        return param_table(beta[0], sigma[0], x_names)
+
+# poisson regression
+def poisson(y, x, data, **kwargs):
     link = lambda x: torch.exp(x)
     loss = lambda yh, y: yh - y*torch.log(yh+eps)
-    return glm(y, x=x, fe=fe, data=data, link_fn=link, loss_fn=loss, **kwargs)
+    return glm(y, x, data, link=link, loss=loss, **kwargs)
+
+# logit regression
+def logit(y, x, data, **kwargs):
+    link = lambda x: torch.exp(x)
+    loss = lambda yh, y: torch.log(1+yh) - y*torch.log(yh+eps)
+    return glm(y, x, data, link=link, loss=loss, **kwargs)
