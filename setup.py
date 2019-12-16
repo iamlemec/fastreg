ab871d0ae8bcc83fdd32396c8719ecb077df3b19
Douglas Hanley <thesecretaryofwar@gmail.com>
2019-12-16 14:33:07 -0500

    requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='fastreg',
-      version='0.1',
-      description='Fast sparse regressions',
-      url='http://github.com/iamlemec/fastreg',
-      author='Doug Hanley',
-      author_email='thesecretaryofwar@gmail.com',
-      license='MIT',
-      packages=['fastreg'],
-      zip_safe=False)
+setup(
+    name='fastreg',
+    version='0.1',
+    description='Fast sparse regressions',
+    url='http://github.com/iamlemec/fastreg',
+    author='Doug Hanley',
+    author_email='thesecretaryofwar@gmail.com',
+    license='MIT',
+    packages=['fastreg'],
+    install_requires=['numpy', 'scipy', 'pandas', 'sklearn'],
+    zip_safe=False
+)
