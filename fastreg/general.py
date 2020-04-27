496022d50f888561c1857740c1ed317e5c11bbe3
Douglas Hanley <thesecretaryofwar@gmail.com>
2020-04-27 16:10:44 -0400

    precompile modelsg

--- fastreg/general.py
+++ fastreg/general.py
@@ -36,6 +36,16 @@ def gammaln(x):
 jax.defjvp(digamma, lambda g, y, x: lax.mul(g, trigamma(x)))
 jax.defjvp(gammaln, lambda g, y, x: lax.mul(g, digamma(x)))
 
+##
+## precompile
+##
+
+inv_fun = jax.jit(np.linalg.inv)
+
+##
+## canned models
+##
+
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
@@ -44,18 +54,39 @@ def log(x):
 
 # link functions
 links = {
-    'identity': lambda x: x,
-    'exponential': lambda x: np.exp(x),
-    'logit': lambda x: 1/(1+np.exp(-x))
+    'ident': lambda x: x,
+    'exp': np.exp,
+    'logit': sigmoid
 }
 
 # loss functions
+def binary_loss(yh, y):
+    return y*log(yh) + (1-y)*log(1-yh)
+
+def poisson_loss(yh, y):
+    return y*log(yh) - yh
+
+def negbin_loss(r, yh, y):
+    return gammaln(r+y) - gammaln(r) + r*log(r) + y*log(yh) - (r+y)*log(r+yh)
+
+def lstsq_loss(yh, y):
+    return -(y-yh)**2
+
 losses = {
-    'binary': lambda yh, y: y*log(yh) + (1-y)*log(1-yh),
-    'poisson': lambda yh, y: y*log(yh) - yh,
-    'negative_binomial': lambda r, yh, y: gammaln(r+y) - gammaln(r) + r*log(r) + y*log(yh) - (r+y)*log(r+yh),
-    'least_squares': lambda yh, y: -(y-yh)**2
+    'binary': lambda p, yh, y: binary_loss(yh, y),
+    'poisson': lambda p, yh, y: poisson_loss(yh, y),
+    'negbin': lambda p, yh, y: negbin_loss(np.exp(p['lr']), yh, y),
+    'lstsq': lambda p, yh, y: lstsq_loss(yh, y)
 }
+
+# modifiers
+def zero_inflate(like0, key='lpzero'):
+    def like(p, yh, y):
+        pzero = sigmoid(p[key])
+        clike = np.clip(like0(p, yh, y), a_max=clip_like)
+        like = pzero*(y==0) + (1-pzero)*np.exp(clike)
+        return log(like)
+    return like
 
 ##
 ## batching it, pytree style
@@ -91,9 +122,10 @@ class DataLoader:
 def chunks(v, n):
     return [v[i:i+n] for i in range(0, len(v), n)]
 
+# need to handle ambiguity in case x.ndim == 1
 def atleast_2d(x, axis=0):
     x2 = np.atleast_2d(x)
-    if x2.shape[axis] == 1:
+    if x.ndim == 1 and axis == 0:
         x2 = x2.T
     return x2
 
@@ -102,9 +134,9 @@ def get_sizes(dms, subset=None):
         subset = list(dms)
     return {k: v.size for k, v in dms.items() if k in subset}
 
-def block_matrix(tree, size):
+def block_matrix(tree, dims, size):
     blocks = chunks(tree_leaves(tree), size)
-    mat = np.block([[atleast_2d(x, axis=int(i2 < i1)) for i2, x in enumerate(row)] for i1, row in enumerate(blocks)])
+    mat = np.block([[atleast_2d(x, axis=int(d>0)) for d, x in zip(dims, row)] for row in blocks])
     return mat
 
 def block_unpack(mat, tree, sizes):
@@ -118,12 +150,13 @@ def block_unpack(mat, tree, sizes):
 ##
 
 # maximum likelihood using jax - this expects a mean log likelihood
-# parameters must be a dictionary of
-def maxlike(model, data, params, c={}, batch_size=4092, epochs=3, learning_rate=0.5, eta=0.001, gamma=0.9, per=100, disp=None, stderr=True):
-    # compute derivatives
-    vg_fun = jax.jit(jax.value_and_grad(model))
-    g_fun = jax.jit(jax.grad(model))
-    h_fun = jax.jit(jax.hessian(model))
+def maxlike(model, data, params, c={}, batch_size=8192, epochs=3, eta=0.01, gamma=0.9, disp=None, stderr=True):
+    # compute derivatives if needed
+    if type(model) is tuple:
+        vg_fun, h_fun = model
+    else:
+        vg_fun = jax.jit(jax.value_and_grad(model))
+        h_fun = jax.jit(jax.hessian(model))
 
     # construct dataset
     data = DataLoader(data, batch_size)
@@ -133,6 +166,7 @@ def maxlike(model, data, params, c={}, batch_size=4092, epochs=3, learning_rate=
     params = tree_map(np.array, params)
     par_flat, par_tree = tree_flatten(params)
     par_sizes = [len(p) if p.ndim > 0 else 1 for p in par_flat]
+    par_dims = [np.ndim(p) for p in par_flat]
     K = len(par_flat)
 
     # track rms gradient
@@ -182,15 +216,39 @@ def maxlike(model, data, params, c={}, batch_size=4092, epochs=3, learning_rate=
         return params, hess
 
     # invert hessian for stderrs
-    hmat = block_matrix(hess, K)
-    ifish = -np.linalg.inv(hmat)/N
+    hmat = block_matrix(hess, par_dims, K)
+    ifish = -inv_fun(hmat)/N
     sigma = block_unpack(ifish, par_tree, par_sizes)
 
     # full standard errors
     return params, sigma
 
+# make a glm model and compile
+def glm_model(link, loss):
+    if type(link) is str:
+        link = links[link]
+    if type(loss) is str:
+        loss = losses[loss]
+
+    # evaluator
+    def model(par, dat):
+        ydat, xdat, cdat = dat['ydat'], dat['xdat'], dat['cdat']
+        real, categ = par['real'], par['categ']
+        linear = xdat @ real
+        for i, c in enumerate(categ):
+            linear += categ[c][cdat[:, i]]
+        pred = link(linear)
+        like = loss(par, pred, ydat)
+        return np.mean(like)
+
+    # compile
+    vg_fun = jax.jit(jax.value_and_grad(model))
+    h_fun = jax.jit(jax.hessian(model))
+
+    return vg_fun, h_fun
+
 # default glm specification
-def glm(y, x=[], fe=[], data=None, extra={}, link=None, loss=None, intercept=True, stderr=True, **kwargs):
+def glm(y, x=[], fe=[], data=None, extra={}, model=None, link=None, loss=None, intercept=True, stderr=True, **kwargs):
     # construct design matrices
     y_vec, x_mat, x_names, c_mat, c_names = design_matrices(y, x=x, fe=fe, data=data, intercept=intercept, method='ordinal')
 
@@ -200,128 +258,60 @@ def glm(y, x=[], fe=[], data=None, extra={}, link=None, loss=None, intercept=Tru
     Kc = len(c_names)
     Kl = [len(c) for c in c_names.values()]
 
-    # evaluator
-    def model(par, dat):
-        ydat, xdat, cdat = dat['ydat'], dat['xdat'], dat['cdat']
-        linear = xdat @ par['beta']
-        for i, c in enumerate(c_names):
-            linear += par[c][cdat[:, i]] - par[c][0]
-        pred = link(linear)
-        like = loss(par, pred, ydat)
-        return np.mean(like)
+    # compile model if needed
+    if model is None:
+        model = glm_model(link, loss)
 
     # displayer
     def disp(e, l, p):
-        beta = p['beta']
-        print(f'[{e:3d}] {l:.4f}: {np.mean(beta):.4f}')
+        real, categ = p['real'], p['categ']
+        mcats = np.array([np.mean(c) for c in categ.values()])
+        print(f'[{e:3d}] {l:.4f}: {np.mean(real):.4f} {np.mean(mcats):.4f}')
 
     # estimate model
     data = {'ydat': y_vec, 'xdat': x_mat, 'cdat': c_mat}
     pcateg0 = {c: np.zeros(s) for c, s in zip(c_names, Kl)}
-    params0 = {'beta': np.zeros(Kx), **pcateg0, **extra}
+    params0 = {'real': np.zeros(Kx), 'categ': pcateg0, **extra}
     beta, sigma = maxlike(model, data, params0, data, stderr=stderr, disp=disp, **kwargs)
 
     return beta, sigma
 
 # logit regression
+logit_model = glm_model(link='logit', loss='binary')
 def logit(y, x=[], fe=[], data=None, **kwargs):
-    link = links['logit']
-    like0 = losses['binary']
-    like = lambda p, yh, y: like0(yh, y)
-    return glm(y, x=x, fe=fe, data=data, link=link, loss=like, **kwargs)
+    return glm(y, x=x, fe=fe, data=data, model=logit_model, **kwargs)
 
 # poisson regression
+poisson_model = glm_model(link='exp', loss='poisson')
 def poisson(y, x=[], fe=[], data=None, **kwargs):
-    link = links['exponential']
-    like0 = losses['poisson']
-    like = lambda p, yh, y: like0(yh, y)
-    return glm(y, x=x, fe=fe, data=data, link=link, loss=like, **kwargs)
+    return glm(y, x=x, fe=fe, data=data, model=poisson_model, **kwargs)
 
 # zero inflated poisson regression
-def zero_inflated_poisson(y, x=[], fe=[], data=None, **kwargs):
-    # base poisson distribution
-    link = links['exponential']
-    like0 = losses['poisson']
+zinf_poisson_model = glm_model(link='exp', loss=zero_inflate(losses['poisson']))
+def zinf_poisson(y, x=[], fe=[], data=None, **kwargs):
     extra = {'lpzero': 0.0}
-
-    # zero inflation
-    def loss(par, yh, y):
-        pzero = sigmoid(par['lpzero'])
-        clike = np.clip(like0(yh, y), a_max=clip_like)
-        like = pzero*(y==0) + (1-pzero)*np.exp(clike)
-        return log(like)
-
-    # pass to glm
-    par, sig = glm(y, x=x, fe=fe, data=data, extra=extra, link=link, loss=loss, **kwargs)
-
-    # transform params
-    par['pzero'] = sigmoid(par.pop('lpzero'))
-
-    return par, sig
+    return glm(y, x=x, fe=fe, data=data, model=zinf_poisson_model, extra=extra, **kwargs)
 
 # negative binomial regression
-def negative_binomial(y, x=[], fe=[], data=None, **kwargs):
-    link = links['exponential']
-    like = losses['negative_binomial']
-    extra = {'lalpha': 0.0}
-
-    def loss(par, yh, y):
-        r = np.exp(-par['lalpha'])
-        return like(r, yh, y)
-
-    # pass to glm
-    par = glm(y, x=x, fe=fe, data=data, extra=extra, link=link, loss=loss, **kwargs)
-
-    # transform params
-    par['alpha'] = np.exp(-par.pop('lalpha'))
-
-    return par
+negbin_model = glm_model(link='exp', loss='negbin')
+def negbin(y, x=[], fe=[], data=None, **kwargs):
+    extra = {'lr': 0.0}
+    return glm(y, x=x, fe=fe, data=data, model=negbin_model, extra=extra, **kwargs)
 
 # zero inflated poisson regression
-def zero_inflated_negative_binomial(y, x=[], fe=[], data=None, **kwargs):
-    # base poisson distribution
-    link = links['exponential']
-    like0 = losses['negative_binomial']
-    extra = {
-        'lpzero': 0.0,
-        'lalpha': 0.0
-    }
-
-    # zero inflation
-    def loss(par, yh, y):
-        pzero = sigmoid(par['lpzero'])
-        r = np.exp(-par['lalpha'])
-        clike = np.clip(like0(r, yh, y), a_max=clip_like)
-        like = pzero*(y==0) + (1-pzero)*np.exp(clike)
-        return log(like)
-
-    # pass to glm
-    par = glm(y, x=x, fe=fe, data=data, extra=extra, link=link, loss=loss, **kwargs)
-
-    # transform params
-    par['pzero'] = sigmoid(par.pop('lpzero'))
-    par['alpha'] = np.exp(-par.pop('lalpha'))
-
-    return par
+zinf_negbin_model = glm_model(link='exp', loss=zero_inflate(losses['negbin']))
+def zinf_negbin(y, x=[], fe=[], data=None, **kwargs):
+    extra = {'lpzero': 0.0, 'lr': 0.0}
+    return glm(y, x=x, fe=fe, data=data, model=zinf_negbin_model, extra=extra, **kwargs)
 
 # ordinary least squares (just for kicks)
-def ordinary_least_squares(y, x=[], fe=[], data=None, **kwargs):
-    # base poisson distribution
-    link = links['identity']
-    loss0 = losses['least_squares']
+def ols_loss(p, yh, y):
+    lsigma = p['lsigma2']
+    sigma2 = np.exp(lsigma2)
+    like = -lsigma2 + lstsq_loss(yh, y)/sigma2
+    return like
+
+ols_model = glm_model(link='ident', loss=ols_loss)
+def ols(y, x=[], fe=[], data=None, **kwargs):
     extra = {'lsigma': 0.0}
-
-    # zero inflation
-    def loss(par, yh, y):
-        lsigma = par['lsigma']
-        sigma2 = np.exp(2*lsigma)
-        like = -lsigma + 0.5*loss0(yh, y)/sigma2
-        return like
-
-    # pass to glm
-    par, sig = glm(y, x=x, fe=fe, data=data, extra=extra, link=link, loss=loss, **kwargs)
-
-    # transform params
-    par['sigma'] = np.exp(par.pop('lsigma'))
-
-    return par, sig
+    return glm(y, x=x, fe=fe, data=data, model=ols_model, extra=extra, **kwargs)
