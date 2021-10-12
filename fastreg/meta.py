31d960ab0dcb3b1f990d607d0c0b50786a935072
Douglas Hanley <thesecretaryofwar@gmail.com>
2021-10-11 22:21:23 -0400

    meta definitions

---
+++ fastreg/meta.py
@@ -0,0 +1,16 @@
+# metaclass structure (helps with autoreload and isinstance)
+
+class MetaFactor:
+    pass
+
+class MetaTerm:
+    pass
+
+class MetaFormula:
+    pass
+
+class MetaReal:
+    pass
+
+class MetaCateg:
+    pass
