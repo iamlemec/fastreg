75ec2fa12a7f3f1ad8dec8f98ceaecac421bc1a7
Douglas Hanley <thesecretaryofwar@gmail.com>
2022-08-18 11:19:32 -0400

    more categorical tests

--- fastreg/tests/test_linear.py
+++ fastreg/tests/test_linear.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 import statsmodels.formula.api as smf
 import fastreg as fr
-from fastreg import I, R, C
+from fastreg import I, R, C, NONE
 
 # standard formulas
 
@@ -18,7 +18,7 @@ x = x0 + C.id1 + C.id2
 
 # helper functions
 
-def rename_statsmodels(s):
+def statsmodels_rename(s):
     if s == 'Intercept':
         return 'I'
     elif ret := re.match(r'C\(([^\)]+)\)\[T\.([^\]]+)\]', s):
@@ -26,47 +26,55 @@ def rename_statsmodels(s):
     else:
         return s
 
+def statsmodels_frame(res):
+    df = pd.DataFrame({
+        'coeff': res.params,
+        'stderr': np.sqrt(np.diagonal(res.cov_params()))
+    })
+    df = df.rename_axis(res.model.endog_names, axis=1)
+    df = df.rename(statsmodels_rename, axis=0)
+    return df
+
+def statsmodels_isclose(fit_fr, fit_sm):
+    ret_fr = fit_fr[['coeff', 'stderr']].sort_index()
+    ret_sm = statsmodels_frame(fit_sm).sort_index()
+    return np.isclose(ret_fr, ret_sm).all()
+
+def frame_valid(df):
+    return ~np.isnan(df).any().any()
+
 # tests
 
 def test_ols_real(data):
     ret = fr.ols(y=y0, x=x0, data=data)
 
-    assert ~np.isnan(ret).any().any()
+    assert frame_valid(ret)
 
 def test_ols_categ(data):
     ret = fr.ols(y=y, x=x, data=data)
 
-    assert ~np.isnan(ret).any().any()
+    assert frame_valid(ret)
 
 def test_ols_real_statsmodels(data):
     fit_fr = fr.ols(y=y0, x=x0, data=data)
     fit_sm = smf.ols('y0 ~ 1 + x1 + x2', data=data).fit()
 
-    ret_fr = fit_fr[['coeff', 'stderr']].sort_index()
-    ret_sm = pd.DataFrame({
-        'coeff': fit_sm.params,
-        'stderr': np.sqrt(np.diagonal(fit_sm.cov_params()))
-    })
-    ret_sm = ret_sm.rename_axis('y0', axis=1)
-    ret_sm = ret_sm.rename(rename_statsmodels, axis=0).sort_index()
-
-    assert np.isclose(ret_fr, ret_sm).all()
+    assert statsmodels_isclose(fit_fr, fit_sm)
 
 def test_ols_categ_statsmodels(data):
-    fit_fr = fr.ols(y=y, x=x, data=data)
-    fit_sm = smf.ols(
+    xp = R.x1 + R.x2 + C.id1(NONE) + C.id2
+    fit0_fr = fr.ols(y=y, x=xp, data=data)
+    fit0_sm = smf.ols(
+        'y ~ 0 + x1 + x2 + C(id1) + C(id2)', data=data, eval_env=-1
+    ).fit()
+
+    fit1_fr = fr.ols(y=y, x=x, data=data)
+    fit1_sm = smf.ols(
         'y ~ 1 + x1 + x2 + C(id1) + C(id2)', data=data, eval_env=-1
     ).fit()
 
-    ret_fr = fit_fr[['coeff', 'stderr']].sort_index()
-    ret_sm = pd.DataFrame({
-        'coeff': fit_sm.params,
-        'stderr': np.sqrt(np.diagonal(fit_sm.cov_params()))
-    })
-    ret_sm = ret_sm.rename_axis('y0', axis=1)
-    ret_sm = ret_sm.rename(rename_statsmodels, axis=0).sort_index()
-
-    assert np.isclose(ret_fr, ret_sm).all()
+    assert statsmodels_isclose(fit0_fr, fit0_sm)
+    assert statsmodels_isclose(fit1_fr, fit1_sm)
 
 def test_absorb(data):
     ret0 = fr.ols(y=y, x=x, data=data)
@@ -96,19 +104,11 @@ def test_hdfe(data):
 def test_ols_robust(data, cov):
     ret = fr.ols(y=y0, x=x0, data=data, stderr=f'hc{cov}')
 
-    assert ~np.isnan(ret).any().any()
+    assert frame_valid(ret)
 
 @pytest.mark.parametrize('cov', [0, 1, 2, 3])
 def test_ols_robust_statsmodels(data, cov):
     fit_fr = fr.ols(y=y0, x=x0, data=data, stderr=f'hc{cov}')
     fit_sm = smf.ols('y0 ~ 1 + x1 + x2', data=data).fit(cov_type=f'HC{cov}')
 
-    ret_fr = fit_fr[['coeff', 'stderr']].sort_index()
-    ret_sm = pd.DataFrame({
-        'coeff': fit_sm.params,
-        'stderr': np.sqrt(np.diagonal(fit_sm.cov_params()))
-    })
-    ret_sm = ret_sm.rename_axis('y0', axis=1)
-    ret_sm = ret_sm.rename(rename_statsmodels, axis=0).sort_index()
-
-    assert np.isclose(ret_fr, ret_sm).all()
+    assert statsmodels_isclose(fit_fr, fit_sm)
